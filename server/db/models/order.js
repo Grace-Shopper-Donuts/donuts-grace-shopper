4e426d02da0b91558827bd8dd803cc0b564ecd54
Robert Fox <robert@foxs.com>
2020-01-14 11:45:16 -0500

    Updated All Models

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,6 +1,12 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 
-const Order = db.define('order')
+const Order = db.define('order', {
+  totalPrice: Sequelize.DECIMAL
+})
+
+// function Order.prototype.totalPrice() = {
+
+// }
 
 module.exports = Order
