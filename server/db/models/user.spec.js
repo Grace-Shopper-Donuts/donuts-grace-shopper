f24dd26ee9137b10ff59c12f4a5dfae978a1d01b
Eric Yang <yangeric12@gmail.com>
2020-01-22 17:21:24 -0500

    update user model and route specs to follow updated user model validations

--- server/db/models/user.spec.js
+++ server/db/models/user.spec.js
@@ -15,7 +15,10 @@ describe('User model', () => {
 
       beforeEach(async () => {
         cody = await User.create({
+          firstName: 'Cody',
+          lastName: 'Codes',
           email: 'cody@puppybook.com',
+          address: '5 Hanover',
           password: 'bones'
         })
       })
